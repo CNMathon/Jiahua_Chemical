427c9e8f760c4e1989eb6d488e1fe763857cb2b7
CNMathon <wsnbtkk@163.com>
2019-11-11 15:59:21 +0800

    daily update

--- src/views/pages/page_3/DuanLu/List.vue
+++ src/views/pages/page_3/DuanLu/List.vue
@@ -16,80 +16,142 @@
       </div>
     </div>
     <j-filter v-model="showFilter" @confirm="confirmFilter">
-      <j-filter-item title="作业票状态" :actions="zypztList" @select="filterSelect_1"></j-filter-item>
+      <j-filter-item title="作业票状态" :actions="zypStatusList" @select="filterSelect_2"></j-filter-item>
       <j-filter-cell title="申请部门"></j-filter-cell>
       <j-filter-cell title="申请人"></j-filter-cell>
     </j-filter>
     <div class="list-card-area">
-      <list-card></list-card>
+      <div class="app">
+        <van-skeleton
+          title
+          :row="3"
+          :loading="isLoading"
+          class="skeleton"
+        >
+          <label v-for="(item, index) in listData" :key="index">
+            <!-- 此处在做完AJAX后需要判断是否为最后行 - class存在判断 -->
+            <div class="donghuo-list-card donghuo-list-card-nolast">
+              <div class="left">
+                <div class="left-line left-line-notlast">许可证编号：{{item.permitCode}}</div>
+                <div class="left-line left-line-notlast">断路原因：{{item.reason}}</div>
+                <div class="left-line left-line-notlast left-line-hor">申请部门：{{item.applyDept}}</div>
+                <div class="left-line left-line-notlast left-line-hor">申请人：{{item.applyer}}</div>
+                <div class="left-line left-line-notlast">作业开始时间：{{item.offtimeStart}}</div>
+                <div class="left-line">作业结束时间：{{item.offtimeEnd}}</div>
+              </div>
+              <div
+                class="right"
+                @click.stop="()=>{$router.push({path:'/page_3/duanlu/index',query:{id:item.id}})}"
+                v-if="item.htStatus == 1"
+              >编辑</div>
+              <div
+                class="right"
+                @click.stop="()=>{$router.push({path:'/page_3/duanlu/index2',query:{id:item.id}})}"
+                v-if="item.htStatus == 2"
+              >初审</div>
+              <div class="right" v-if="item.htStatus == 3">审核</div>
+              <div class="right" v-if="item.htStatus == 4">有效</div>
+              <div class="right" v-if="item.htStatus == 5">结束</div>
+            </div>
+          </label>
+        </van-skeleton>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mixin } from "@/mixin/mixin";
-import ListCard from "@/views/pages/page_3/components/DuanLuListCard";
+// import ListCard from "@/views/pages/page_3/components/DuanLuListCard";
 export default {
-  created(){
-
-    this.getPageData()
-
+  created() {
+    this.getPageData();
   },
   data() {
     return {
       showFilter: false,
       searchValue: "",
       searchValues: "",
-      zypztList: [
-        {
-          name: "编辑",
-          idnex: 1
-        },
-        {
-          name: "初审",
-          idnex: 2
-        },
-        {
-          name: "有效",
-          idnex: 3
-        },
-        {
-          name: "已验票",
-          idnex: 4
-        },
-        {
-          name: "已终结",
-          idnex: 5
-        }
-      ] // 作业票状态列表
+      zypztList: [ // 作业票状态列表
+        {index: -1, name: "请选择"},
+        {index: 1, name: "修理马路"},
+        {index: 2, name: "开挖埋设水管"},
+        {index: 3, name: "埋设电缆"},
+        {index: 4, name: "开挖沟渠"},
+        {index: 5, name: "吊装设备"},
+        {index: 6, name: "管道"}
+      ],
+      zypStatusList: [
+        {index: -1, name: "请选择"},
+        {index: 1, name: "编辑"},
+        {index: 2, name: "初审"},
+        {index: 3, name: "审核"},
+        {index: 4, name: "有效"},
+        {index: 5, name: "已终结"},
+      ],
+      isLoading: true,
+      listData: [],
+      reason: '',
+      status: '',
     };
   },
   mixins: [mixin],
   methods: {
+    // 点击右侧文案事件
     onClickRight() {
       this.$router.push({
         path: "../duanlu"
       });
-
     },
-    getPageData() {
 
+    // 获取页面数据
+    getListData() {
+      this.isLoading = true
       this.$api.page_3
         .htHseDlzypListData({
           __sid: localStorage.getItem("JiaHuaSessionId"),
+          reason: this.reason,
+          htStatus:this.status
         })
         .then(res => {
-           console.log(res)
-        }).catch(()=>{});
+          this.listData = res.list;
+          this.isLoading = false
+          console.log(this.listData)
 
+        });
     },
-    confirmFilter() {},
-    filterSearch() {},
-    filterSelect_1() {}
+    // 确认筛选
+    confirmFilter() {
+      this.getListData();
+    },
+    
+    // 选择断路原因
+    filterSelect_1(e) {
+      console.log(e)
+      if (e.index == -1) {
+        this.reason = ""
+      }
+      else {
+        this.reason = e.index
+      }
+      this.getListData();
+    },
+
+    // 选择状态
+    filterSelect_2(e) {
+      console.log(e)
+      if (e.index == -1) {
+        this.status = ""
+      }
+      else {
+        this.status = e.index
+      }
+    }
+
   },
-  components: {
-    ListCard
-  }
+  created() {
+    this.getListData()
+  },
 };
 </script>
 
@@ -110,6 +172,51 @@ export default {
   justify-content: space-between;
   /deep/ .cell-sheet {
     width: 680px;
+  }
+}
+</style>
+
+<!-- List Card -->
+<style lang="scss" scoped>
+.donghuo-list-card {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: #6096f8;
+  padding: 22px 31px 15px 34px;
+  color: #ffffff;
+  border-radius: 10px;
+}
+
+.donghuo-list-card-nolast {
+  margin-bottom: 20px;
+}
+
+.left-line-notlast {
+  margin-bottom: 7px;
+}
+
+.left {
+  font-size: 0.8rem;
+  font-family: PingFangSC-Regular, PingFang SC;
+}
+
+.right {
+  line-height: 80 * 2px;
+  font-size: 34px;
+  width: 112px;
+  text-align: center;
+  // color: red;
+}
+
+.skeleton {
+  margin-bottom: 10px
+}
+
+.left-line-hor {
+  display: inline;
+  &:nth-child(4) {
+    margin-left: 50px;
   }
 }
 </style>
--- src/views/pages/page_3/KongJian/List.vue
+++ src/views/pages/page_3/KongJian/List.vue
@@ -15,7 +15,7 @@
       @search="getPageData('refresh')"
       @tap="showFilter = true"
     ></j-filter-bar>
-    <j-filter v-model="showFilter" @confirm="confirmFilter">
+    <j-filter v-model="showFilter" @confirm="getPageData">
       <j-filter-search v-model="searchValues" @search="filterSearch"></j-filter-search>
       <j-filter-item title="作业票状态" :actions="zypztList" @select="filterSelect_1"></j-filter-item>
       <j-filter-cell title="申请部门"></j-filter-cell>
@@ -28,27 +28,26 @@
           :row="3"
           :loading="isLoading"
           class="skeleton"
-          v-for="(item, index) in 3"
-          :key="index"
-        ></van-skeleton>
-        <label v-for="(item, index) in listData" :key="index">
-          <!-- 此处在做完AJAX后需要判断是否为最后行 - class存在判断 -->
-          <div class="donghuo-list-card donghuo-list-card-nolast" @click="jumpToMorePage(item.htStatus, item.sxkjCode)">
-            <div class="left">
-              <div class="left-line left-line-notlast">作业内容：{{item.sxkjNeurogen}}</div>
-              <div class="left-line left-line-notlast left-line-hor">申请部门：{{item.applyDept}}</div>
-              <div class="left-line left-line-notlast left-line-hor">申请人：{{item.applyRen}}</div>
-              <div class="left-line left-line-notlast">作业开始时间：{{item.zyStarttime}}</div>
-              <div class="left-line">作业结束时间：{{item.zyEndtime}}</div>
+        >
+          <label v-for="(item, index) in listData" :key="index">
+            <!-- 此处在做完AJAX后需要判断是否为最后行 - class存在判断 -->
+            <div class="donghuo-list-card donghuo-list-card-nolast" @click="jumpToMorePage(item.htStatus, item.sxkjCode)">
+              <div class="left">
+                <div class="left-line left-line-notlast">作业内容：{{item.sxkjNeurogen}}</div>
+                <div class="left-line left-line-notlast left-line-hor">申请部门：{{item.applyDept}}</div>
+                <div class="left-line left-line-notlast left-line-hor">申请人：{{item.applyRen}}</div>
+                <div class="left-line left-line-notlast">作业开始时间：{{item.zyStarttime}}</div>
+                <div class="left-line">作业结束时间：{{item.zyEndtime}}</div>
+              </div>
+              <div class="right" v-if="item.htStatus == 1">编辑</div>
+              <div class="right" v-if="item.htStatus == 2">初审</div>
+              <div class="right" v-if="item.htStatus == 3">审核</div>
+              <div class="right" v-if="item.htStatus == 4">审批</div>
+              <div class="right" v-if="item.htStatus == 5">有效</div>
+              <div class="right" v-if="item.htStatus == 6">终结</div>
             </div>
-            <div class="right" v-if="item.htStatus == 1">编辑</div>
-            <div class="right" v-if="item.htStatus == 2">初审</div>
-            <div class="right" v-if="item.htStatus == 3">审核</div>
-            <div class="right" v-if="item.htStatus == 4">审批</div>
-            <div class="right" v-if="item.htStatus == 5">有效</div>
-            <div class="right" v-if="item.htStatus == 6">终结</div>
-          </div>
-        </label>
+          </label>
+        </van-skeleton>
       </div>
     </div>
   </div>
@@ -64,40 +63,31 @@ export default {
       searchValue: "",
       searchValues: "",
       zypztList: [
-        {
-          name: "编辑",
-          idnex: 1
-        },
-        {
-          name: "初审",
-          idnex: 2
-        },
-        {
-          name: "有效",
-          idnex: 3
-        },
-        {
-          name: "已验票",
-          idnex: 4
-        },
-        {
-          name: "已终结",
-          idnex: 5
-        }
+        {index: -1, name: "请选择"},
+        {index: 1, name: "编辑"},
+        {index: 2, name: "初审"},
+        {index: 3, name: "审核"},
+        {index: 4, name: "审批"},
+        {index: 5, name: "有效"},
+        {index: 6, name: "终结"}
       ], // 作业票状态列表
       listData: [],
-      isLoading: true
+      isLoading: true,
+      status: ""
     };
   },
   mixins: [mixin],
   methods: {
     getPageData() {
+      this.isLoading = true
       this.$api.page_3
         .htHseSxkjzypListData({
           zyContent: this.searchValue,
+          htStatus: this.status,
           __sid: localStorage.getItem("JiaHuaSessionId")
         })
         .then(res => {
+          this.isLoading = false
           this.listData = res.list;
           this.isLoading = false;
           console.log(this.listData);
@@ -136,7 +126,15 @@ export default {
     },
     confirmFilter() {},
     filterSearch() {},
-    filterSelect_1() {}
+    // 选择断路原因
+    filterSelect_1(e) {
+      if (e.index == -1) {
+        this.status = ""
+      }
+      else {
+        this.status = e.index
+      }
+    },
   },
   created() {
     this.getPageData()
